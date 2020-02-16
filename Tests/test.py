e691ab79a294c9d152ddf8c83970b9ed5ec4ad08
SrManny <supermanubros-85@hotmail.com>
2020-02-16 18:05:12 +0100

    Host parametrizado

--- Tests/test.py
+++ Tests/test.py
@@ -9,17 +9,19 @@ import unittest
 import requests
 import json
 
-class TestPhotoslurp(unittest.TestCase):
 
+class TestPhotoslurp(unittest.TestCase):
+    host = "http://10.0.75.1:5000/"
     def test_postWorking(self):
         url = "http://funcionaElPost.com"
-        r = requests.post("http://10.0.75.1:5000/Photoslurp/importByURL",data ={"url":url})
+        
+        r = requests.post(self.host+"Photoslurp/importByURL",data ={"url":url})
         ResultUrl = json.loads(r.text)['url']
         self.assertEqual(ResultUrl,url, "Should be " + url)
     
     def test_postCheckURLWorking(self):
         url = "httpqweqwr"
-        r = requests.post("http://10.0.75.1:5000/Photoslurp/importByURL",data ={"url":url})
+        r = requests.post(self.host+"Photoslurp/importByURL",data ={"url":url})
         result = json.loads(r.text)
         self.assertIn("error", result, "We get the error: " + result['error'])
         self.assertEqual("url wrong format",result['error'], "We get the correct error.")
@@ -28,15 +30,15 @@ class TestPhotoslurp(unittest.TestCase):
         url1 = "https://www.indiandcold.com/media/photo_export.csv"
         url2 = "http://commondatastorage.googleapis.com/newfeedspec/example_feed_txt.zip"
         url3 = "https://factionskis.com/pages/photoslurp-product-feed"
-        r = requests.post("http://127.0.0.1:5000/Photoslurp/importByURL",data ={"url":url1})
+        r = requests.post(self.host+"/Photoslurp/importByURL",data ={"url":url1})
         result = json.loads(r.text)['format']
         self.assertEqual("csv",result, "We get the correct format.")
         
-        r = requests.post("http://10.0.75.1:5000/Photoslurp/importByURL",data ={"url":url2})
+        r = requests.post(self.host+"Photoslurp/importByURL",data ={"url":url2})
         result = json.loads(r.text)['error']
         self.assertEqual("File format unknown or not recognizable.",result, "We get the correct format.")
         
-        r = requests.post("http://10.0.75.1:5000/Photoslurp/importByURL",data ={"url":url3})
+        r = requests.post(self.host+"/Photoslurp/importByURL",data ={"url":url3})
         result = json.loads(r.text)['format']
         self.assertEqual("xml",result, "We get the correct format.")
     
